40cafc9a92e98594f426a1c9f3d9b0b711db4290
andryluthfi <uthe_strife99@yahoo.co.id>
2014-07-14 21:14:49 +0700

    tes load TPS dan import semua model

--- protected/controllers/InputController.php
+++ protected/controllers/InputController.php
@@ -5,6 +5,14 @@
  * @author Andry Luthfi
  */
 class InputController extends ControllerCommon {
-
+    
+    public function actionAdventure() {
+        $tps = $this->prioritizeTPS();
+        $this->render('adventure');
+    }
+    
+    protected function prioritizeTPS() {
+        return TPS::model()->find(array('order'=>'rand()'));
+    }
 
 }
